b8002ddee15d0ce5a2946cb672e9d4c8b54404da
Michael Radaszkiewicz <mrada6@gmail.com>
2020-05-23 19:52:41 -0400

    working model

--- runner.py
+++ runner.py
@@ -9,33 +9,59 @@ import matplotlib.image as mpimg
 import numpy as np
 from keras.preprocessing import image
 
+import numpy as np
+import itertools
+import keras
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from keras.models import Sequential
+from keras import optimizers
+from keras.preprocessing import image
+from keras.layers import Dropout, Flatten, Dense
+from keras import applications
+from keras.utils.np_utils import to_categorical
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import math
+import datetime
+import time
 
+
+x = 185
+y =260
+channel = 3
 
 
 print(tf.__version__)
 
 # All images will be rescaled by 1./255
-train_datagen = ImageDataGenerator(rescale=1/255)
-validation_datagen = ImageDataGenerator(rescale=1/255)
+train_datagen = ImageDataGenerator(rescale=1.0/255.0)
+validation_datagen = ImageDataGenerator(rescale=1.0/255.0)
+
+batch_size = 20
 
 train_generator = train_datagen.flow_from_directory(
     './tmp/set/Training',  # This is the source directory for training images
-    target_size=(300, 300),  # All images will be resized to 150x150
-    batch_size=128,
+    target_size=(x, y),  # All images will be resized to 150x150
+    batch_size=batch_size,
     # Since we use binary_crossentropy loss, we need binary labels
     class_mode='categorical')
 
 validation_generator = train_datagen.flow_from_directory(
     './tmp/set/Validation',  # This is the source directory for training images
-    target_size=(300, 300),  # All images will be resized to 150x150
-    batch_size=128,
+    target_size=(x, y),  # All images will be resized to 150x150
+    batch_size=batch_size,
     # Since we use binary_crossentropy loss, we need binary labels
     class_mode='categorical')
+
+nb_train_samples = len(train_generator.filenames)
+num_classes = len(train_generator.class_indices)
+
+predict_size_train = int(math.ceil(nb_train_samples / batch_size))
 
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(64, (3,3), activation='relu',
-                           input_shape=(300, 300, 3)),
+                           input_shape=(x,y,channel)),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
@@ -43,23 +69,61 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D(2,2),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(validation_generator.num_classes, activation='relu')
+    tf.keras.layers.Dense(validation_generator.num_classes, activation='softmax')
 ])
+
+
 
 #print out what the model looks like
 model.summary()
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(lr=0.001),
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
+modelDesc='model_64-32-16'
+checkpoint_path = "training_3/"+modelDesc+".ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
 
 history = model.fit(
     train_generator,
     validation_data = validation_generator,
     epochs=15,
-    steps_per_epoch=8,
-    validation_steps=8,
-    verbose=1)
+    steps_per_epoch=93,
+    validation_steps=10,
+    callbacks=[cp_callback],)
+
+import pathlib
+pathlib.Path("/trained").mkdir(parents=True, exist_ok=True)
+
+model.save("/trained/model_"+modelDesc+".h5")
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend()
+plt.show()
 
 print("done")
